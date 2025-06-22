8fd7f8ccb44ddfcc5cafce481a7a56ab310a9779
James Goudreault <nsp023@gmail.com>
2025-06-22 15:30:36 -0700

    added docker

--- app.py
+++ app.py
@@ -35,6 +35,26 @@ def create_store():
     stores[store_id] = store
     return store, 201
 
+@app.delete("/store/<string:store_id>")
+def delete_store(store_id):
+    try:
+        del stores[store_id]
+        return {"message": "Store deleted."}
+    except KeyError:
+        abort(404, message="Store not found.")
+
+
+@app.get("/item")
+def get_all_items():
+    return {"items": list(items.values())}
+
+@app.get("/item/<string:item_id>")
+def get_item(item_id):
+    try:
+        return items[item_id]
+    except KeyError:
+        return abort(404, message="Item not found.")
+
 @app.post("/item")
 def create_item():
     item_data = request.get_json()
@@ -48,29 +68,41 @@ def create_item():
             message="Bad Request Ensure 'price', 'store_id', and 'name' are included in the JSON payload."
         )
 
-    if item_data["store_id"] not in stores:
-        return abort(404, message="Store not found.")
-
-    for item in item_data.values():
+    for item in items.values():
         if(
             item_data["name"] == item["name"]
             and item_data["store_id"] == item["store_id"]
         ):
-            abort(400, message=f"Item {item_data["name"]} already exists.")
+            abort(400, message=f"Item '{item["name"]} already exists.")
+ 
+    if item_data["store_id"] not in stores:
+        return abort(404, message="Store not found.")
 
     item_id = uuid.uuid4().hex
     item = {**item_data, "id": item_id}
     items[item_id] = item
-    
+
     return item, 201
 
-@app.get("/item")
-def get_all_items():
-    return {"items": list(items.values())}
+@app.put("/item/<string:item_id>")
+def update_item(item_id):
+    item_data = request.get_json()
+    if "price" not in item_data or "name" not in item_data:
+        abort(400, message="Bad request, ensure price and name in in the item data.")
 
-@app.get("/item/<string:item_id>")
-def get_item(item_id):
     try:
-        return items[item_id]
+        item = items[item_id]
+        item |= item_data
+
+        return item
+    except KeyError:
+        abort(404, message="Item not found.")
+
+@app.delete("/item/<string:item_id>")
+def delete_item(item_id):
+    try:
+        del items[item_id]
+        return {"message": "Item deleted."}
     except KeyError:
         return abort(404, message="Item not found.")
+    
--- db.py
+++ db.py
@@ -3,9 +3,11 @@ items = {
     1:{
         "name": "Chair",
         "price": 17.99,
+        "store_id": ""
     },
-    2: {
+    2:{
         "name": "Table",
-        "price": 180.50
+        "price": 180.50,
+        "store_id": ""
     }
 }
